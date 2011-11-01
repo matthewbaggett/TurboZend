dd7baac096290bb9b27f475423ddc31a2b94f8a5
geusebio <geusebio@c29433ff-1b1d-4321-a7e5-4b4d3a6625c3>
2011-11-01 12:04:48 +0000

    Adding Turbo_Db_Table_Abstract
    git-svn-id: http://svn.turbocrms.com/code/libraries/Turbo@138 c29433ff-1b1d-4321-a7e5-4b4d3a6625c3

---
+++ Db/Table/Abstract.php
@@ -0,0 +1,15 @@
+<?php
+
+class Turbo_Db_Table_Abstract extends Zend_Db_Table_Abstract
+{
+	static public function factory(){
+		$called_name = get_called_class();
+		return new $called_name();
+	}
+	public function select($withFromPart = self::SELECT_WITHOUT_FROM_PART){
+		$select = parent::select($withFromPart);
+		$select->where('bolDeleted = 0');
+		return $select;
+	}
+}
+
