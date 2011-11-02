04d956f2905f778184d5fcb7b05460559ed0d0fe
geusebio <geusebio@c29433ff-1b1d-4321-a7e5-4b4d3a6625c3>
2011-11-02 17:43:46 +0000

    Begining to make objects aware of their relatives
    git-svn-id: http://svn.turbocrms.com/code/libraries/Turbo@154 c29433ff-1b1d-4321-a7e5-4b4d3a6625c3

--- Db/Table/Row/Base.php
+++ Db/Table/Row/Base.php
@@ -15,4 +15,49 @@ class Turbo_Db_Table_Row_Base extends Zend_Db_Table_Row_Abstract{
 			return TRUE;
 		}
 	}
+	
+	public function __turbo_has_virtual_method($method){
+		if($this->__call_get_method($method) !== FALSE){
+			return TRUE;
+		}else{
+			return FALSE;
+		}
+	}
+	
+	protected function __call_get_method($method){
+		if (preg_match('/^([get|set]+)_(.*)/', $method, $matches)) {
+            $index  = null;
+            $action = $matches[1];
+            $variable = $matches[2];
+            if(isset($this->variable_map[$variable])){
+            	$arr_action = array('action' => $action, 'variable' => $variable);
+            	return $arr_action;	
+            }else{
+            	return FALSE;
+            }
+		}
+		return FALSE;
+	}
+	
+	protected function __call_do_action($action, $variable, $arguments){
+		
+		switch($action){
+			case 'get':
+				$str_column = $this->variable_map[$variable];
+				return $this->$str_column;
+				break;
+			case 'set':
+				$str_column = $this->variable_map[$variable];
+				$this->$str_column = $arguments[0];
+				break;
+		}
+	}
+	
+	public function __call($method, $arguments){
+		
+		if($arr_action = $this->__call_get_method($method)){
+			return $this->__call_do_action($arr_action['action'], $arr_action['variable'], $arguments);
+		}
+		return parent::__call($method, $arguments);
+	}
 }
