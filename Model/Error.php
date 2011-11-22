cf61deb4180febf29b3838e80d3735045b050621
geusebio <geusebio@c29433ff-1b1d-4321-a7e5-4b4d3a6625c3>
2011-11-22 22:30:48 +0000

    Adding exception logging to DB
    git-svn-id: http://svn.turbocrms.com/code/libraries/Turbo@215 c29433ff-1b1d-4321-a7e5-4b4d3a6625c3

---
+++ Model/Error.php
@@ -0,0 +1,5 @@
+<?php 
+class Turbo_Model_Error extends Zend_Db_Table_Row_Abstract{
+	
+	
+}
--- Models/Error.php
+++
@@ -1,7 +0,0 @@
-<?php 
-class Application_Model_Favourite extends Zend_Db_Table_Row_Abstract{
-	
-	public function getUser(){
-		return Application_Model_User::getUser($this->intUserID);
-	}
-}
