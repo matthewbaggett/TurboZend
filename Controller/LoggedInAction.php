e0ff885ea2a195b805989d94b4bb4736bd0ce845
geusebio <geusebio@c29433ff-1b1d-4321-a7e5-4b4d3a6625c3>
2012-04-23 13:20:25 +0000

    git-svn-id: http://svn.turbocrms.com/code/libraries/Turbo@807 c29433ff-1b1d-4321-a7e5-4b4d3a6625c3

--- Controller/LoggedInAction.php
+++ Controller/LoggedInAction.php
@@ -1,4 +1,9 @@
 <?php 
 class Turbo_Controller_LoggedInAction extends Turbo_Controller_Action{
-	
+	public function __construct(){
+		if(! Turbo_Model_User::getCurrentUser()){
+			header("Location: /Login");
+			exit;
+		}
+	}
 }
