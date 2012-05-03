44f3ef977280f15ede87f586d95d12343d202a2f
geusebio <geusebio@c29433ff-1b1d-4321-a7e5-4b4d3a6625c3>
2012-05-03 13:34:18 +0000

    git-svn-id: http://svn.turbocrms.com/code/libraries/Turbo@888 c29433ff-1b1d-4321-a7e5-4b4d3a6625c3

--- Controller/LoggedInAction.php
+++ Controller/LoggedInAction.php
@@ -3,9 +3,13 @@ class Turbo_Controller_LoggedInAction extends Turbo_Controller_Action{
 	public function __construct(Zend_Controller_Request_Abstract $request,
                                 Zend_Controller_Response_Abstract $response,
                                 array $invokeArgs = array()){
-		if(! Turbo_Model_User::getCurrentUser()){
-			header("Location: /Login");
-			exit;
+		if (PHP_SAPI == 'cli'){
+			//Skip all checks.
+		}else{
+			if(! Turbo_Model_User::getCurrentUser()){
+				header("Location: /Login");
+				exit;
+			}
 		}
 		return parent::__construct($request,$response,$invokeArgs);
 	}
