e080545c98f3c1ae3a7f949fd929ff0afb8f3564
geusebio <geusebio@c29433ff-1b1d-4321-a7e5-4b4d3a6625c3>
2012-06-10 12:28:29 +0000

    Adding SimpleTest just to get SimpleBrowser..
    git-svn-id: http://svn.turbocrms.com/code/libraries/Turbo@1108 c29433ff-1b1d-4321-a7e5-4b4d3a6625c3

--- Lib/Browser.php
+++ Lib/Browser.php
@@ -1,5 +1,5 @@
 <?php
-require_once(dirname(__FILE__) . "/PhpQuery/phpQuery.php") ;
+require_once(dirname(__FILE__) . "/SimpleTest/browser.php") ;
 class Turbo_Lib_Browser{
 	
 	/**
@@ -7,10 +7,9 @@ class Turbo_Lib_Browser{
 	 * @return phpQueryObject
 	 */
 	static function get($url){
-		return phpQuery::browserGet('http://www.google.com/', function($browser){ 
-			var_dump($browser);
-			return $browser; 
-		});
+		$browser = &new SimpleBrowser();
+		$browser->get($url);
+		return $browser;
 	}
 	
 	
