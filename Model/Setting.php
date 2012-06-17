8381cf9ac656abdc705004fb96a7fb947f5d8024
geusebio <geusebio@c29433ff-1b1d-4321-a7e5-4b4d3a6625c3>
2012-06-17 18:12:23 +0000

    git-svn-id: http://svn.turbocrms.com/code/libraries/Turbo@1170 c29433ff-1b1d-4321-a7e5-4b4d3a6625c3

--- Model/Setting.php
+++ Model/Setting.php
@@ -6,15 +6,27 @@ class Turbo_Model_Setting extends Zend_Db_Table_Row_Abstract{
 		$sel->where('strName = ?', $setting_name);
 		$o_setting = $tblSettings->fetchRow($sel);
 		if($o_setting){
-			return $o_setting;
+			return unserialize($o_setting->strValue);
 		}else{
-			self::set_setting($setting_name,null);
-			return self::get($setting_name);
+			self::set($setting_name,null);
+			return unserialize(self::get($setting_name)->strValue);
 		}
 	}
 
-	static public function set_setting($setting_name,$setting_value = null){
-
+	static public function set($setting_name,$setting_value = null){
+		$tblSettings = new Turbo_Model_DbTable_Settings();
+		$sel = $tblSettings->select(true);
+		$sel->where('strName = ?', $setting_name);
+		$o_setting = $tblSettings->fetchRow($sel);
+		if($o_setting){
+			$o_setting->strValue = serialize($setting_value);
+			$o_setting->save();
+		}else{
+			$tblSettings->insert(array(
+				'strName' => $setting_name,
+				'strValue' => serialize($setting_value)
+			));
+		}
 	}
 
 }
