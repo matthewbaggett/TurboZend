5441f9d83cd45c00a85d25d13043ff702e2ae621
geusebio <geusebio@c29433ff-1b1d-4321-a7e5-4b4d3a6625c3>
2012-06-17 05:53:06 +0000

    git-svn-id: http://svn.turbocrms.com/code/libraries/Turbo@1166 c29433ff-1b1d-4321-a7e5-4b4d3a6625c3

--- Model/Error.php
+++ Model/Error.php
@@ -1,20 +1,5 @@
 <?php 
-class Turbo_Model_Setting extends Zend_Db_Table_Row_Abstract{
-	static public function get($setting_name){
-		$tblSettings = new Turbo_Model_DbTable_Settings();
-		$sel = $tblSettings->select(true);
-		$sel->where('strName = ?', $setting_name);
-		$o_setting = $tblSettings->fetchRow($sel);
-		if($o_setting){
-			return $o_setting;
-		}else{
-			self::set_setting($setting_name,null);
-			return self::get($setting_name);
-		}
-	}
+class Turbo_Model_Error extends Zend_Db_Table_Row_Abstract{
 	
-	static public function set_setting($setting_name,$setting_value = null){
-		
-	}
 	
 }
--- Model/Setting.php
+++ Model/Setting.php
@@ -1,5 +1,20 @@
-<?php 
-class Turbo_Model_Error extends Zend_Db_Table_Row_Abstract{
-	
-	
+<?php
+class Turbo_Model_Setting extends Zend_Db_Table_Row_Abstract{
+	static public function get($setting_name){
+		$tblSettings = new Turbo_Model_DbTable_Settings();
+		$sel = $tblSettings->select(true);
+		$sel->where('strName = ?', $setting_name);
+		$o_setting = $tblSettings->fetchRow($sel);
+		if($o_setting){
+			return $o_setting;
+		}else{
+			self::set_setting($setting_name,null);
+			return self::get($setting_name);
+		}
+	}
+
+	static public function set_setting($setting_name,$setting_value = null){
+
+	}
+
 }
